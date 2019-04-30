3f8cdfa734a0053f06a050ea969b8c34e64e2933
poikilos <7557867+poikilos@users.noreply.github.com>
2019-04-30 17:38:55 -0400

    make keyboard layout change instant

--- utilities/keyboard-layouts/us/colemak-nonroot.sh
+++ utilities/keyboard-layouts/us/colemak-nonroot.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
+colemak_x_dst="/usr/local/bin/colemak_x.sh"
+if [ -f "$colemak_x_dst" ]; then
+    sh $colemak_x_dst
+else
+    echo "* ERROR: missing $colemak_x_dst - run colemak.sh first"
+fi
 gsettings set org.gnome.desktop.input-sources sources "[('xkb','us+colemak'),('xkb','us')]"
