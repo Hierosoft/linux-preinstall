cee735b090075d20f5b5b54d52e8e6ad0ad79cf4
poikilos <7557867+poikilos@users.noreply.github.com>
2019-07-24 19:02:24 -0400

    account for renamed pygame related to deprecated py2

--- everyone-ubuntu/design.sh
+++ everyone-ubuntu/design.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 sudo apt-add-repository ppa:kivy:kivy-team/kivy
-sudo apt-get install -y blender librecad filezilla gimp xsane pygame pygame-python3  openshot
-
+sudo apt install -y blender librecad filezilla gimp xsane openshot
+#sudo apt install -y pygame
+#sudo apt install -y pygame-python3
+sudo apt install -y python-pygame
