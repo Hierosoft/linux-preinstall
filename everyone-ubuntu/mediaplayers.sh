5cbc8106d518e58d743e18c1118b340da3dda688
poikilos <7557867+poikilos@users.noreply.github.com>
2019-07-24 18:54:01 -0400

    separate some packages missing from default Mint 19.1 repos

--- everyone-ubuntu/mediaplayers.sh
+++ everyone-ubuntu/mediaplayers.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 sudo apt-add-repository ppa:ubuntuhandbook1/dvdstyler
-sudo apt-get update
-sudo apt-get install -y vlc timidity timidity-interfaces-extra lmms vorbis libvorbis lame flac audex mplayer gmplayer dvdisaster dvdstyler dvdstyler-data mjpegtools brasero pulseaudio pulseaudio-utils pavucontrol gnome-alsamixer libimobiledevice-utils
+sudo apt update
+sudo apt install -y vlc timidity timidity-interfaces-extra lmms lame flac audex mplayer dvdisaster mjpegtools brasero pulseaudio pulseaudio-utils pavucontrol gnome-alsamixer libimobiledevice-utils
+sudo apt install -y vorbis libvorbis
+sudo apt install -y gmplayer
+sudo apt install dvdstyler dvdstyler-data
+
