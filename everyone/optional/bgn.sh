f23d36a5820a0edbaf873b15b615a3e0dba31eb6
poikilos <7557867+poikilos@users.noreply.github.com>
2019-07-31 16:33:34 -0400

    fix URL bug for BackupGoNow web installer

--- everyone/optional/bgn.sh
+++ everyone/optional/bgn.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 customDie() {
-	echo
-	echo "ERROR:"
-	echo "$1"
-	echo
-	exit 1
+    echo
+    echo "ERROR:"
+    echo "$1"
+    echo
+    exit 1
 }
 cd
 if [ ! -d "git" ]; then
     mkdir git
 fi
 cd ~/git || customDie "Cannot cd $HOME/git"
-ffsUrl=https://github.com/poikilos/ForwardFileSync.git
+ffsURL=https://github.com/poikilos/ForwardFileSync.git
 
 if [ ! -d "ForwardFileSync" ]; then
     echo "getting ForwardFileSync..."
-    git clone $ffsUrl || customDie "Cannot clone $ffsUrl"
+    git clone $ffsURL || customDie "Cannot clone $ffsURL"
 else
     cd ForwardFileSync
     echo "updating ForwardFileSync..."
     git pull || echo "Cannot update ForwardFileSync"
 fi
 cd ~/git || customDie "Cannot cd $HOME/git"
+bgnURL=https://github.com/poikilos/BackupGoNow
 if [ ! -d "BackupGoNow" ]; then
     echo "Getting BackupGoNow..."
-    git clone $ffsUrl || customDie "Cannot clone $ffsUrl"
+    git clone $bgnURL || customDie "Cannot clone $bgnURL"
     cd "$HOME/git/BackupGoNow" || customDie "Cannot cd $HOME/git/BackupGoNow"
 else
     echo "updating BackupGoNow..."
