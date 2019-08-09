a8647c2211cb11eaa9580863ff99c3c44d6e1cd2
poikilos <7557867+poikilos@users.noreply.github.com>
2019-08-09 10:17:39 -0400

    update package naming and availability for Bionic

--- everyone-ubuntu/development.sh
+++ everyone-ubuntu/development.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
-sudo apt-get install monodevelop python3-kivy kivy-examples geany automake autoconf git mercurial qcake
+sudo apt install -y python3-kivy
+sudo apt install -y geany
+sudo apt install -y automake autoconf
+sudo apt install -y git
+sudo apt install -y mercurial
+sudo apt install -y mono-complete
+
+# Packages no longer available as of 18.04:
+# - monodevelop: see mono-complete instead
+# - kivy-examples
+# - qcake
+
 #some common development packages are in UNIVERSE including: mercurial maven2 qcake
 
 #qcake: 3D game maker for PLIB(TM) (you can release games with QCake-Player)
