ade7df6a6afd531d0f018dbdc11560d2b66227c0
poikilos <7557867+poikilos@users.noreply.github.com>
2019-08-09 10:58:17 -0400

    Add rebootif, 2.80 binary release installer, add-on notes

---
+++ everyone/blender-2.8-release.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+customDie() {
+    echo
+    echo "ERROR:"
+    echo "$1"
+    echo
+    echo
+    exit 1
+}
+
+installPath=`pwd`
+release_version=2.80
+dlName=blender-$release_version-linux-glibc217-x86_64.tar.bz2
+exName=blender-$release_version-linux-glibc217-x86_64
+url=https://mirror.clarkson.edu/blender/release/Blender$release_version/$dlName
+blenders=$HOME/Downloads/blendernightly/versions
+
+if [ ! -d "$HOME/Downloads" ]; then
+    mkdir "$HOME/Downloads" || customDie "Cannot mkdir '$HOME/Downloads'"
+fi
+
+
+if [ ! -f "$HOME/Downloads/$dlName" ]; then
+    wget -O "$HOME/Downloads/$dlName" $url || customDie "Cannot download '$url'"
+else
+    echo "Using existing $HOME/Downloads/$dlName..."
+fi
+
+cd "$HOME/Downloads" || customDie "Cannot cd '$HOME/Downloads'"
+
+if [ ! -d "$blenders" ]; then
+    mkdir -p "$blenders"
+fi
+if [ -d "$exName" ]; then
+    echo "Removing old '`pwd`/$exName'..."
+    rm -Rf "$exName"
+fi
+echo "Extracting..."
+tar xf $dlName || customDie "Cannot extract '$dlName'"
+if [ ! -f "`command -v rsync`" ]; then
+    customDie "This script requires rsync."
+fi
+if [ ! -d "$exName" ]; then
+    customDie "Extracting $dlName did not result in `pwd`/$exName."
+fi
+rsync -rt "$exName" "$blenders"
+
+if [ -z "$PREFIX" ]; then
+    PREFIX=$HOME/.local
+fi
+if [ -z "$SHARE" ]; then
+    SHARE=$PREFIX/share
+fi
+applications_path=$SHARE/applications
+iconName=org.blender.blender-current.desktop
+standard_icon_path=$applications_path/$iconName
+
+installed_bin="$blenders/$exName/blender"
+if [ ! -f "$installed_bin" ]; then
+    customDie "rsync did not result in $installed_bin"
+fi
+srcShortcut="$installPath/share/applications/org.blender.blender-current.desktop"
+if [ ! -f "$srcShortcut" ]; then
+    echo "The shortcut is missing: '`pwd`/$srcShortcut'."
+fi
+if [ ! -d "$applications_path" ]; then
+    mkdir -p "$applications_path"
+fi
+tmpSC=/tmp/$iconName
+cat "$srcShortcut" | grep -v Name= | grep -v Exec= > $tmpSC
+echo "Name=Blender $release_version (Current)" >> $tmpSC
+echo "Exec=$installed_bin" >> $tmpSC
+mv -f $tmpSC "$applications_path/"
+if [ ! -f "$applications_path/$iconName" ]; then
+    customDie "Generating shortcut failed: '$applications_path/$iconName'"
+else
+    echo "Added shortcut: '$applications_path/$iconName'"
+fi
--- everyone/blender-addons.sh
+++ everyone/blender-addons.sh
@@ -1,19 +1,59 @@
 #!/bin/sh
 
-echo "This script"
+#echo "This script"
 ../utilities/blender-install-git-addon.sh https://github.com/machin3io/MACHIN3tools.git
+
+cat <<END
+There are some addons on the Blender Market you may find helpful for
+simplifying the modeling of advanced shapes:
+- [Boxcutter](https://blendermarket.com/products/boxcutter)
+  Price: ~$20
+  Description: Easily add many shapes of divots and protrusions, such
+    as using freehand shape drawing and arrays.
+- [Bezier Mesh Shaper](https://blendermarket.com/products/bezier-mesh-shaper)
+  Price: ~9.99
+  Description: Deform parts of a mesh using bezier curves.
+- [EdgeFlow](https://blenderartists.org/t/it-is-finally-here-edge-flow-set-flow-for-blender-benjamin-saunder/1128115)
+  Price: free of charge
+  Description: Reflow multiple edge loops, making spacing and curve smooth.
+  Install: "../utilities/blender-install-git-addon.sh https://github.com/BenjaminSauder/EdgeFlow.git"
+  Alternatives: mifthtools (formerly Miratools)
+- [Hardops](https://blendermarket.com/products/hardopsofficial)
+  Price: ~$20
+  Description: hard surface modeling shortcuts
+- Meshmachine ~~https://blendermarket.com/products/MESHmachine~~
+  Price: name your price
+  Description: no longer on Blender Market
+    See instead:
+    <https://gumroad.com/l/MESHmachine#>
+- [mifthtools](https://blenderartists.org/t/miratools/637385)
+  Price: Free
+  Install: "../utilities/blender-install-git-addon.sh https://github.com/mifth/mifthtools"
+  Description: (formerly Mira Tools) Linear deform, curve stretch,
+    model along surfaces (see also [mifthtools
+    wiki](https://github.com/mifth/mifthtools/wiki))
+- [Sculpt Toolkit](https://blendermarket.com/products/sculpttkt---a-tool-kit-for-sculptors)
+  Price: ~$15
+  Rating: 4/5
+  Description: Metaball-like but detailed modeling
+  Alternatives: Speedsculpt
+- [Speedsculpt](https://blendermarket.com/products/speedsculpt)
+  Price: ~$15
+  Rating: 5/5
+  Description: Metaball-like but detailed modeling, cut curves,
+    and other features for highly-detailed models. Add eyebrows or hair
+    by drawing a freehand outline on the surface.
+  Alternatives: Sculpt Toolkit
+END
 
 cat <<END
 Manually install and enable:
 - Pie menus (works well with MACHIN3tools)
+  - built into Blender >=2.8
 
 Manually enable built-in:
 - Loop Tools
 - TinyCAD
 - 3D Print Toolbox
-
-Paid ones that may be helpful:
-- HardOps: hard surface modeling shortcuts
-- boxcutter: https://blendermarket.com/products/boxcutter
 
 END
