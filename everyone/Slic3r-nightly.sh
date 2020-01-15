6575132cf3d8de97ac5c79541fc7209ef019c17d
poikilos <7557867+poikilos@users.noreply.github.com>
2020-01-14 23:17:30 -0500

    fix path usage in Slic3r-nightly

--- everyone/Slic3r-nightly.sh
+++ everyone/Slic3r-nightly.sh
@@ -11,13 +11,14 @@ installer_path=../utilities/install_any.py
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 linux_preinstall_repo_path="$( dirname $DIR )"
-if [ ! -f "$installer_path" ]; then
+#if [ ! -f "$installer_path" ]; then
+# always get absolute path
     # See https://stackoverflow.com/questions/59895/get-the-source-directory-of-a-bash-script-from-within-the-script-itself
     try_path="$linux_preinstall_repo_path/utilities/install_any.py"
     if [ -f "$try_path" ]; then
         installer_path="$try_path"
     fi
-fi
+#fi
 if [ ! -f "$installer_path" ]; then
     customDie "You must run this from the utilities directory so \"$installer_path\" can be found."
 fi
@@ -28,4 +29,5 @@ cd ~/Downloads || customDie "'cd ~/Downloads' failed."
 latest_name=Slic3r-master-latest.AppImage
 url=https://dl.slic3r.org/dev/linux/$latest_name
 wget -O $latest_name $url || customDie "'wget $url' failed."
-python "$installer_path" "$latest_name" || customDie "'python \"$installer_path\" \"$latest_name\"' failed."
+latest_path="`pwd`/$latest_name"
+python3 "$installer_path" "$latest_path" || customDie "'python \"$installer_path\" \"$latest_name\"' failed."
