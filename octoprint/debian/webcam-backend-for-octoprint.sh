cb413c9a9f4cc471767104f4990d8179d2da3492
poikilos <7557867+poikilos@users.noreply.github.com>
2020-01-16 16:55:27 -0500

    add uvcdynctrl to webcam backend

--- octoprint/debian/webcam-backend-for-octoprint.sh
+++ octoprint/debian/webcam-backend-for-octoprint.sh
@@ -15,13 +15,32 @@ echo "The scripts named webcam-backend* are only necessary for using a webcam (b
 # See https://community.octoprint.org/t/setting-up-octoprint-on-a-raspberry-pi-running-raspbian/2337
 
 apt update
-echo
-echo "WARNING: This would add about 509MB on Debian 10. To continue, type:"
-# NOTE: subversion package DOES install git
-echo "  apt install -y subversion libjpeg62-turbo-dev imagemagick ffmpeg libv4l-dev cmake"
-echo "  # if you still can't type the 'git' command successfully after that, try:"
-echo "  apt install -y git"
-echo
+apt install -y uvcdynctrl
+cat <<END
+* Installed uvcdynctrl for controlling the webcam parameters such as brightness (see <http://www.linux-commands-examples.com/uvcdynctrl>):
+
+  # Show current setting:
+  uvcdynctrl -d /dev/video0 -g "Brightness"
+
+  # Change setting:"
+  uvcdynctrl -d /dev/video0 -g "Brightness"
+
+  # List all settings (some are locked down to a certain range of
+  # values-- or you'll get unexpected error 22. (Code: 12) ).
+  # You may also have to toggle "" to use "" (tested on GUCEE HD92-720P webcam):
+  uvcdynctrl -s
+
+NOTE: The subversion package should install git.
+If you still can't type the 'git' command successfully after entering the command under the warning, try:
+  apt install -y git
+
+WARNING: The backend below would add about 509MB on Debian 10.
+
+  To continue, type:
+  apt install -y subversion libjpeg62-turbo-dev imagemagick ffmpeg libv4l-dev cmake
+
+
+END
 usermod -a -G video $UNPRIV_USER
 
 # See https://unix.stackexchange.com/questions/56957/how-to-start-an-application-automatically-on-boot
