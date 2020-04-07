4e325258166e626226c0a23914df560727cbe9aa
poikilos <7557867+poikilos@users.noreply.github.com>
2020-04-07 11:31:17 -0400

    Handle more versions of PHP. Clarify output. Suppress apt prompts.

--- server/optional/php-set-version.apache.sh
+++ server/optional/php-set-version.apache.sh
@@ -14,9 +14,9 @@ if [ -z "$1" ]; then
 fi
 ver=$1
 # next 9 lines are from https://docs.nextcloud.com/server/14/admin_manual/installation/source_installation.html#ubuntu-installation-label
-sudo apt install apache2 mariadb-server libapache2-mod-php$ver
-sudo apt install php$ver-gd php$ver-json php$ver-mysql php$ver-curl php$ver-mbstring
-sudo apt install php$ver-intl php-imagick php$ver-xml php$ver-zip
+sudo apt install -y apache2 mariadb-server libapache2-mod-php$ver
+sudo apt install -y php$ver-gd php$ver-json php$ver-mysql php$ver-curl php$ver-mbstring
+sudo apt install -y php$ver-intl php-imagick php$ver-xml php$ver-zip
 sudo a2enmod rewrite
 sudo a2enmod headers
 sudo a2enmod env
@@ -24,16 +24,33 @@ sudo a2enmod dir
 sudo a2enmod mime
 sudo a2enmod ssl
 
-sudo a2dismod php7.0
-sudo a2dismod php7.1
-sudo a2dismod php7.2
-sudo a2dismod php7.3
+echo "* disabling old modules..."
+# for OTHER_VER in 7.0 7.1 7.2 7.3 7.4 7.5 7.6 7.7 7.8 7.9
+# do
+#     sudo a2dismod php$OTHER_VER 1>/dev/null 2>&1
+# done
+# List modules: sudo apachectl -M | awk '{print $1}' | grep php
+# List modules without _modules suffix:
+# sudo apachectl -M | awk '{print $1}' | awk -F_ '{print $1}' | grep php
+module_count=0
+for MODULE_NAME in `sudo apachectl -M | awk '{print $1}' | awk -F_ '{print $1}' | grep php`
+do
+    sudo a2dismod $MODULE_NAME 1>/dev/null 2>&1
+    module_count=$((module_count+1))
+    if [ $? -eq 0 ]; then
+        echo "- a2dismod $MODULE_NAME...OK"
+    else
+        echo "- a2dismod $MODULE_NAME...FAILED"
+    fi
+done
+echo "  - done ($module_count PHP modules)"
+
 echo "enabling php$ver..."
 sudo a2enmod php$ver
-sudo apt install php$ver-pgsql
-# sudo apt install php$ver-sqlite3
-sudo apt install php$ver-opcache
-sudo apt install php$ver-readline
-sudo apt install php$ver-memcached
+sudo apt install -y php$ver-pgsql
+# sudo apt install -y php$ver-sqlite3
+sudo apt install -y php$ver-opcache
+sudo apt install -y php$ver-readline
+sudo apt install -y php$ver-memcached
 sudo update-alternatives --set php /usr/bin/php$ver
 sudo systemctl restart apache2
