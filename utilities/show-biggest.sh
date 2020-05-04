efb978b733cf61c4565685a6cec1c2428d4e5793
poikilos <7557867+poikilos@users.noreply.github.com>
2020-05-04 16:38:51 -0400

    Allow passing a location to show-biggest.

--- utilities/show-biggest.sh
+++ utilities/show-biggest.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
-outfile=~/biggest.txt
-sudo du -Sh / | sort -rh | head -n 100 > $outfile
+BIG_LIST_FILE=~/biggest.txt
+THIS_CMD_PREFIX=
+if [ ! -z "$1" ]; then
+    BIG_DIR="$1"
+fi
+if [ -z "$BIG_DIR" ]; then
+    BIG_DIR=/
+    THIS_CMD_PREFIX=sudo
+fi
+$THIS_CMD_PREFIX du -Sh "$BIG_DIR" | sort -rh | head -n 100 > $BIG_LIST_FILE
 echo
 echo
-echo "Done (see $outfile)."
+echo "Done (see $BIG_LIST_FILE)."
 echo "If it is empty, try:"
-echo "  rm '$outfile'"
+echo "  rm '$BIG_LIST_FILE'"
 echo "- Then run this script as root."
 echo
 echo
