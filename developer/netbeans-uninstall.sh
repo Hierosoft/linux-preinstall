53f1f79a85cb0cb2308abe50b713ca5364a73d05
poikilos <7557867+poikilos@users.noreply.github.com>
2020-05-12 20:27:35 -0400

    Work on netbeans install on Fedora 32.

---
+++ developer/netbeans-uninstall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ "@$USER" = "@root" ]; then
+	/usr/local/netbeans-8.2/uninstall.sh
+else
+	
+fi
---
+++ developer/netbeans.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+me=`basename "$0"`
+sudo dnf install -y wget java-1.8.0-openjdk java-1.8.0-openjdk-devel
