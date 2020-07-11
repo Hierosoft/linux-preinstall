93b852ecdaeae0a9dca005cce7ffd332de977008
poikilos <7557867+poikilos@users.noreply.github.com>
2020-07-11 12:51:23 -0400

    Try harder to reduce PackageKit memory. See description for the issue.
    https://bugzilla.redhat.com/show_bug.cgi?id=1354074

--- everyone/disable-plasma-pk-updates.sh
+++ everyone/disable-plasma-pk-updates.sh
@@ -8,3 +8,10 @@
 # ^ don't disable packagekit. Also don't remove it, as the following depend on it:
 #  gnome-software inkscape kde-print-manager plasma-applet-redshift-control plasma-breeze plasma-discover plasma-pk-updates sddm-breeze simple-scan
 dnf remove -y plasma-pk-updates
+# ^ even after that, packagekitd sometimes takes >10GB RAM after some time
+echo "ShutdownTimeout=300" | sudo tee -a /etc/PackageKit/PackageKit.conf
+# -a: append
+systemctl restart packagekit
+
+# Not tried yet:
+# dnf remove -y PackageKit
