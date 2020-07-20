2bdbfbe36f27cb43294ffa35cf49e8ecf39ca3aa
poikilos <7557867+poikilos@users.noreply.github.com>
2020-07-19 22:09:42 -0400

    Add deps notes from DarkWav Minecraft-Installer-RPM's readme.

--- everyone/minecraft-nonroot.sh
+++ everyone/minecraft-nonroot.sh
@@ -4,6 +4,27 @@ echo
 echo "INFO: Choose the Java Edition if your OS is 32-bit."
 echo
 sleep 5
+
+if [ ! -f "`command -v rpmbuild`" ]; then
+    echo "You must have rpmbuild"
+    exit 1
+fi
+
+if [ ! -f "`command -v wget`" ]; then
+    echo "You must have wget"
+    exit 1
+fi
+
+if [ ! -f "`command -v tar`" ]; then
+    echo "You must have tar"
+    exit 1
+fi
+
+if [ ! -f "`command -v bash`" ]; then
+    echo "You must have bash"
+    exit 1
+fi
+
 if [ ! -f "`rpm`" ]; then
     echo "Only rpm-based distros are available currently."
     echo "For deb-based distros, use the official deb."
