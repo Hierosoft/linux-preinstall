0bf4508f122a6fede53ebe7bb9b8bd45ecfba112
poikilos <7557867+poikilos@users.noreply.github.com>
2020-11-12 16:21:56 -0500

    Add nfs share creator script.

---
+++ server/nfs-share_owner_videos.fedora.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# See [NFS : Configure NFS Server](https://www.server-world.info/en/note?os=Fedora_31&p=nfs&f=1)
+# and [5.4.3. DO NOT USE THE NO_ROOT_SQUASH OPTION](https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/4/html/security_guide/s2-server-nfs-noroot)
+dnf install -y nfs-utils
+firewall-cmd --add-service=nfs --permanent
+firewall-cmd --add-service={nfs3,mountd,rpc-bind} --permanent
+firewall-cmd --reload
+DOMAIN_LINE="Domain = home.local"
+echo "$DOMAIN_LINE"
+SHARE_LINE="/home/owner/Videos 192.168.1.0/24(rw,anonuid=1000,anongid=1000)"
+echo "If the line above isn't right, press Ctrl+C to exit now and write a better line to /etc/exports manually (then add a line like '$SHARE_LINE' to /etc/exports and run 'systemctl enable --now rpcbind nfs-server')."
+echo "3..."
+sleep 1
+echo "2..."
+sleep 1
+echo "1..."
+sleep 1
+echo "$DOMAIN_LINE" >> /etc/idmapd.conf
+echo "$SHARE_LINE"
+echo "If the line above isn't right, press Ctrl+C to exit now and write a better line to /etc/exports manually (then run 'systemctl enable --now rpcbind nfs-server')."
+echo "3..."
+sleep 1
+echo "2..."
+sleep 1
+echo "1..."
+sleep 1
+echo "$SHARE_LINE" >> /etc/exports
+systemctl enable --now rpcbind nfs-server
