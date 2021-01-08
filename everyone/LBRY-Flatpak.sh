feabbf01add4641b7dba0adacc62662a54312dd1
poikilos <7557867+poikilos@users.noreply.github.com>
2021-01-07 19:07:52 -0500

    Show a flatpak permissions fix in case it previously ran with sudo.

--- everyone/LBRY-Flatpak.sh
+++ everyone/LBRY-Flatpak.sh
@@ -4,10 +4,18 @@ mkdir -p ~/tmp
 cd ~/tmp
 fname=io.lbry.lbry-app.flatpakref
 wget -O $fname https://dl.flathub.org/repo/appstream/$fname
+
 flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 # ^ as per <https://flatpak.org/setup/Fedora/> + --user
 flatpak install --user -y flathub io.lbry.lbry-app
 # ^ as per <https://flathub.org/apps/details/io.lbry.lbry-app> + --user -y
+if [ $? -neq 0 ]; then
+    echo "The flatpak install command failed as $USER."
+    echo "Try the following (if no sudo run as root) then try again:"
+    echo "sudo chown -R $USER:$USER $HOME/.local/share/flatpak"
+    exit 1
+fi
+
 flatpak update --user -y io.lbry.lbry-app
 
 # The following is not necessary since flatpak makes an icon
