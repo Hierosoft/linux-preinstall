b0b2a8045dd23276bab11cefbdd4547a0d11cd7d
poikilos <7557867+poikilos@users.noreply.github.com>
2021-02-23 01:06:08 -0500

    Fix a type-o of requested_ver & remove use of sudo in the script. Rename a script that seems to be Ubuntu-specific.

--- server/optional/php-set-version.nginx.deb.sh
+++ server/optional/php-set-version.nginx.deb.sh
@@ -2,7 +2,7 @@
 
 # see also https://github.com/nextcloud/server/issues/11638#issuecomment-483868535
 
-sudo systemctl status nginx
+systemctl status nginx
 code=$?
 if [ $code -ne 0 ]; then
     echo
@@ -22,11 +22,11 @@ CHANGE=true
 if [ "@$2" = "@CHECKUP" ]; then
     CHANGE=false
 fi
-reqested_ver=$1
+requested_ver=$1
 # The next few lines are not for apache but are based on
 # https://docs.nextcloud.com/server/14/admin_manual/installation/source_installation.html#ubuntu-installation-label
 if [ "@$CHANGE" = "@true" ]; then
-    sudo apt install -y mariadb-server
+    apt install -y mariadb-server
 fi
 
 
@@ -35,11 +35,11 @@ NL=$'\n'
 WARNING_SITES=""
 for ver in 5.0 5.1 5.2 5.3 5.3 5.4 5.5 5.6 5.7 5.8 5.9 6.0 6.1 6.2 6.3 6.4 6.5 6.6 6.7 6.8 6.9 7.0 7.1 7.2 7.3 7.4 7.5 7.6 7.7 7.8 7.9 8.0 8.1 8.2 8.3 8.4 8.5 8.6 8.7 8.8 8.9 9.0
 do
-if [ "$reqested_ver" != "$ver" ]; then
+if [ "$requested_ver" != "$ver" ]; then
     if [ "@$CHANGE" = "@true" ]; then
-        sudo apt remove -y php$ver-gd php$ver-json php$ver-mysql php$ver-curl php$ver-mbstring
-        sudo apt remove -y php$ver-intl php-imagick php$ver-xml php$ver-zip
-        sudo apt remove -y php$ver-fpm
+        apt remove -y php$ver-gd php$ver-json php$ver-mysql php$ver-curl php$ver-mbstring
+        apt remove -y php$ver-intl php-imagick php$ver-xml php$ver-zip
+        apt remove -y php$ver-fpm
     fi
     for SITE in `ls -t /etc/nginx/sites-available`
     do
@@ -75,43 +75,43 @@ if [ "@$CHANGE" = "@true" ]; then
 fi
 echo
 echo "The following list should be empty (dpkg --get-selections | grep -i php): START"
-sudo dpkg --get-selections | grep -i php
+dpkg --get-selections | grep -i php
 echo "END"
 echo
 
-ver=$reqested_ver
+ver=$requested_ver
 if [ "@$CHANGE" = "@true" ]; then
-    sudo apt install -y php$ver-gd php$ver-json php$ver-mysql php$ver-curl php$ver-mbstring
-    sudo apt install -y php$ver-intl php-imagick php$ver-xml php$ver-zip
+    apt install -y php$ver-gd php$ver-json php$ver-mysql php$ver-curl php$ver-mbstring
+    apt install -y php$ver-intl php-imagick php$ver-xml php$ver-zip
     # ^ This installs php-imagick php5.6-imagick php7.0-imagick php7.1-imagick php7.2-imagick php7.2-intl php7.2-xml php7.2-zip php7.3-imagick
     #     php7.4-imagick php8.0-imagick ttf-dejavu-core
     #   for some reason.
 
 
-    sudo apt install -y php$ver-fpm
-    sudo systemctl enable php$ver-fpm
-    sudo systemctl start php$ver-fpm
-    sudo systemctl status php$ver-fpm
+    apt install -y php$ver-fpm
+    systemctl enable php$ver-fpm
+    systemctl start php$ver-fpm
+    systemctl status php$ver-fpm
 
     # NOTE: at this point, the other services should already be masked,
     # so don't bother doing anything to them.
 
     # For WordPress plugins:
     ## As of 2020-04-01, sury is required for php7.4-mbstring (See https://github.com/wyveo/nginx-php-fpm/blob/master/Dockerfile)
-    echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > sudo tee /etc/apt/sources.list.d/php.list
-    sudo apt update
-    sudo apt install -y php$ver-mbstring
+    echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > tee /etc/apt/sources.list.d/php.list
+    apt update
+    apt install -y php$ver-mbstring
 
 
     echo "enabling php$ver..."
-    sudo apt install php$ver-pgsql
-    # sudo apt install php$ver-sqlite3
-    sudo apt install php$ver-opcache
-    sudo apt install php$ver-readline
-    sudo apt install php$ver-memcached
-    sudo update-alternatives --set php /usr/bin/php$ver
-    # sudo systemctl reload nginx
-    sudo systemctl restart nginx
+    apt install php$ver-pgsql
+    # apt install php$ver-sqlite3
+    apt install php$ver-opcache
+    apt install php$ver-readline
+    apt install php$ver-memcached
+    update-alternatives --set php /usr/bin/php$ver
+    # systemctl reload nginx
+    systemctl restart nginx
 fi
 if [ ! -z "$WARNING_SITES" ]; then
     echo "WARNING: The following sites in /etc/nginx/sites-available still try to call old versions of php:"
