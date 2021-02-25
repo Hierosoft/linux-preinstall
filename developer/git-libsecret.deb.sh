37a230cf1f24393410d12fe0829f1087c9e63719
poikilos <7557867+poikilos@users.noreply.github.com>
2021-02-25 14:19:38 -0500

    Improve git-credential-libsecret instructions.

--- developer/git-libsecret.deb.sh
+++ developer/git-libsecret.deb.sh
@@ -9,3 +9,12 @@ sudo make --directory=/usr/share/doc/git/contrib/credential/libsecret
 # git-credential-libsecret.c  Makefile"
 git config --global credential.helper \
    /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret
+
+cat <<END
+libsecret uses gnome-keyring. Git credentials are saved to ~/.git-credentials so to shred them run:
+
+shred -u ~/.git-credentials
+
+
+For more information or to use pass (for GnuPG) instead, see <https://my-take-on.tech/2019/08/23/safely-storing-git-credentials/>.
+END
