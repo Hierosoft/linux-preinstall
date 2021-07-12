cf99b78d2b8a74c8d777b44c25ef75ea9af72f9d
poikilos <7557867+poikilos@users.noreply.github.com>
2021-07-12 13:39:44 -0400

    Add Geeqie JP2 viewer.

--- developer/developer.sh
+++ developer/developer.sh
@@ -2,4 +2,7 @@
 flatpak install flathub \
     org.mapeditor.Tiled \
     com.github.PintaProject.Pinta \
+    org.geeqie.Geeqie \
     ;
+
+# Geeqie: This image viewer can view JPEG 2000 (JP2) files.
