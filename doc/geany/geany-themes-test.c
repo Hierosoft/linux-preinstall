83e671159a22b7d02695b0ea8e4c85aa41bd0cfe
poikilos <7557867+poikilos@users.noreply.github.com>
2021-11-22 20:31:13 -0500

    Create and upload Epsilon Dark color scheme for Geany

---
+++ doc/geany/geany-themes-test.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define MESSAGE "Hello World"
+
+/* Prints a message to standard output */
+void print_message(const char *msg) {
+	printf("%s\n", msg);
+}
+
+int main(int argc, char *argv[]) {
+	print_message(MESSAGE);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* This code is only for testing the geany color scheme by opening this
+file in Geany and matches the sample code at
+https://github.com/codebrainz/geany-themes/tree/master/screenshots#readme
+for the purpose of visual comparison with the themes there */
---
+++ doc/geany/readme.md
@@ -0,0 +1,10 @@
+# linux-preinstall Geany components
+
+## Epsilon Dark Theme
+The files are located at https://github.com/poikilos/linux-preinstall/tree/master/AlwaysAdd/HOME/.config/geany
+
+Installation:
+```
+git clone https://github.com/poikilos/linux-preinstall.git linux-preinstall
+cd linux-preinstall && mkdir -p ~/.config/geany/colorschemes && cp ./AlwaysAdd/HOME/.config/geany/colorschemes/epsilon-dark.conf ~/.config/geany/colorschemes/
+```
