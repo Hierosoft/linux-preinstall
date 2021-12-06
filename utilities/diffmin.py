ed260d3bcbda3b794dd6752bcb5ade631003b1b7
poikilos <7557867+poikilos@users.noreply.github.com>
2021-12-05 20:02:59 -0500

    Fix mixed spaces and tabs. Rename the error print function. Add a docstring.

--- utilities/diffmin.py
+++ utilities/diffmin.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
+'''
+Show lines in the second file that aren't in the first file to stdout.
+All output that isn't file content goes to stderr.
+'''
 from __future__ import print_function
 import sys
 
 # see <https://stackoverflow.com/questions/5574702/how-to-print-to-stderr-in-python>
-def eprint(*args, **kwargs):
+def prerr(*args, **kwargs):
+    # PRint eRRoR
     print(*args, file=sys.stderr, **kwargs)
 
 if len(sys.argv) < 3:
-    eprint("You must specify two files.")
+    prerr("You must specify two files.")
     exit(1)
 
 # NOTE: sys.argv[0] is this script.
@@ -16,24 +21,24 @@ docDict = {}
 count = 0
 with open(fName, 'r') as f:
     for lineOriginal in f:
-	if not lineOriginal: break
-	line = lineOriginal.strip()
-	docDict[line] = True
-	count += 1
-eprint("- read " + str(count) + " line(s)")
+        if not lineOriginal: break
+        line = lineOriginal.strip()
+        docDict[line] = True
+        count += 1
+prerr("- read " + str(count) + " line(s)")
 
 f2Name = sys.argv[2]
 
 count = 0
 newCount = 0
 with open(f2Name, 'r') as f:
-    eprint("Reading " + f2Name + "...")
+    prerr("Reading " + f2Name + "...")
     for lineOriginal in f:
-	line = lineOriginal.strip()
-	if docDict.get(line) is None:
-	    print(lineOriginal.rstrip())
-	    newCount += 1
-	count += 1
-eprint("- read " + str(count) + " line(s)")
-eprint("- " + str(newCount) + " new line(s).")
+        line = lineOriginal.strip()
+        if docDict.get(line) is None:
+            print(lineOriginal.rstrip())
+            newCount += 1
+        count += 1
+prerr("- read " + str(count) + " line(s)")
+prerr("- " + str(newCount) + " new line(s).")
 
