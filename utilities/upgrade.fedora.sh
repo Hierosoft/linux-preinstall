a3f1dd2e5eda3980c84edbeb5692e6edd5182240
poikilos <7557867+poikilos@users.noreply.github.com>
2022-04-21 12:07:14 -0400

    Improve the Fedora upgrade script.

--- utilities/upgrade.fedora.sh
+++ utilities/upgrade.fedora.sh
@@ -1,8 +1,31 @@
-if [ -z "$1" ]; then
-    echo "You must specify the next Fedora version."
+#!/bin/bash
+. /etc/os-release
+if [ "$NAME" != "Fedora" ]; then
+    echo "Error: NAME=Fedora was expected in /etc/os-release but NAME is $NAME. This script is only for Fedora."
     exit 1
 fi
-sudo dnf upgrade -y --refresh
-sudo dnf install -y dnf-plugin-system-upgrade
-sudo dnf system-upgrade download -y --releasever=$1
-#&& sudo dnf system-upgrade reboot
+me=`basename $0`
+NEXT_VER=$(($VERSION_ID+1))
+cat <<END
+[$me] This script does:
+dnf upgrade --refresh -y && dnf install dnf-plugin-system-upgrade -y && dnf system-upgrade download -y --releasever=$1
+# dnf system-upgrade reboot
+END
+if [ -z "$1" ]; then
+    echo "[$me] You must specify a version."
+    if [ ! -z "$VERSION_ID" ]; then
+        echo "[$me] You have $VERSION_ID so you should specify $NEXT_VER."
+    else
+        echo "[$me] Also, $VERSION_ID was expected in /etc/os-release but the variable wasn't present."
+    fi
+    exit 1
+fi
+
+dnf upgrade --refresh -y && dnf install dnf-plugin-system-upgrade -y && dnf system-upgrade download -y --releasever=$1
+
+echo <<END
+[$me] The following manual steps are necessary if everything worked so far:
+
+dnf system-upgrade reboot
+
+END
