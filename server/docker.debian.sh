df9e128854ead6e8af539d3bbfa935734c703df9
poikilos <7557867+poikilos@users.noreply.github.com>
2022-05-11 11:14:20 -0400

    Get docker working on Devuan.

---
+++ server/docker.debian.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# See <https://docs.docker.com/engine/install/debian/>:
+
+apt-get remove -y docker
+apt-get remove -y docker.io
+apt-get remove -y containerd
+apt-get remove -y runc
+apt-get remove -y docker-engine
+# ^ Not on Devuan Chimera (based on Debian 10 Buster)
+apt-get update
+apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# "Use the following command to set up the stable repository. To add
+# the nightly or test repository, add the word nightly or test (or both)
+# after the word stable in the commands below. Learn about nightly and
+# test channels."
+
+lsb_release_name="$(lsb_release -cs)"
+if [ "@$lsb_release_name" = "@chimera" ]; then
+    # Devuan Chimera is based on Debian Buster
+    lsb_release_name=buster
+fi
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+   $lsb_release_name stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+if [ $? -ne 0 ]; then
+    # "Your default umask may not be set correctly, causing the
+    # public key file for the repo to not be detected. Run the following
+    # command and then try to update your repo again:"
+    chmod a+r /usr/share/keyrings/docker-archive-keyring.gpg
+fi
+apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+# "Install a specific version using the version string from the second
+# column, for example, 5:18.09.1~3-0~debian-stretch .
+# sudo apt-get install docker-ce=<VERSION_STRING> docker-ce-cli=<VERSION_STRING> containerd.io docker-compose-plugin
+docker run hello-world
