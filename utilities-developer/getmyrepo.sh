3f0bcc0a206711019847bf2bd7c6d5f2d98388dc
poikilos <7557867+poikilos@users.noreply.github.com>
2022-06-20 22:44:33 -0400

    Respect all parameters (such as --site) and pass them along to getrepo.sh.

--- utilities-developer/getmyrepo.sh
+++ utilities-developer/getmyrepo.sh
@@ -10,7 +10,7 @@ if [ -z "$1" ]; then
     "You must specify a repo name."
 fi
 if [ ! -z "$FORCE_GIT_DIR" ]; then
-    getrepo.sh $1 --user $MY_GITHUB_USERNAME --repos_dir "$FORCE_GIT_DIR" --user_dir "$FORCE_GIT_DIR"
+    getrepo.sh "$@" --user $MY_GITHUB_USERNAME --repos_dir "$FORCE_GIT_DIR" --user_dir "$FORCE_GIT_DIR"
 else
-    getrepo.sh $1 --user $MY_GITHUB_USERNAME --repos_dir "`pwd`" --user_dir "`pwd`"
+    getrepo.sh "$@" --user $MY_GITHUB_USERNAME --repos_dir "`pwd`" --user_dir "`pwd`"
 fi
