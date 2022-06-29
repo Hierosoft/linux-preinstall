36c894b9d3797cd0fcb088ea78e01d97e8803147
poikilos <7557867+poikilos@users.noreply.github.com>
2022-06-28 21:07:30 -0400

    Add citations.

--- drivers/nvidia-screen-tearing-fix.sh
+++ drivers/nvidia-screen-tearing-fix.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-In the file, write the following line:
 
+# As per <https://ubuntuhandbook.org/index.php/2018/07/fix-screen-tearing-ubuntu-18-04-optimus-laptops/>
+# (as discussed at <https://askubuntu.com/a/1252499>):
 destConf="/etc/modprobe.d/nvidia-drm-nomodeset.conf"
 printf "* "
 if [ -f "$destConf" ]; then
@@ -13,6 +14,9 @@ sudo update-initramfs -u
 echo
 echo "xrandr output:"
 xrandr
+
+# modeset wasn't enough, so as per
+# [Nvidia Drivers - Really NASTY screen tearing](https://forums.linuxmint.com/viewtopic.php?t=282653):
 cat <<END
 Now reboot and run (requires proprietary NVIDIA driver and you still may need to turn on vsync and buffered both in the NVIDIA settings GUI):
   sudo nvidia-settings --assign CurrentMetaMode="DVI-I-1:1920x1080_60 +0+0 { ForceFullCompositionPipeline = On }"
