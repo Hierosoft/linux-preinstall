f5ef59be96d0d40755b48dac45b643f7d906bcde
poikilos <7557867+poikilos@users.noreply.github.com>
2022-08-05 16:00:01 -0400

    Add a missing import.

--- setup.py
+++ setup.py
@@ -1,18 +1,11 @@
 import setuptools
 import sys
-# - For the example on which this was based, see
-#   https://github.com/poikilos/world_clock/blob/main/setup.py
-#   which is based on
-#   https://github.com/poikilos/nopackage/blob/main/setup.py
-#   which is based on
-#   https://github.com/poikilos/pypicolcd/blob/master/setup.py
-# - For nose, see https://github.com/poikilos/mgep/blob/master/setup.py
+import os
 
 '''
-python_mr = sys.version_info.major
 versionedModule = {}
 versionedModule['urllib'] = 'urllib'
-if python_mr == 2:
+if sys.version_info.major < 3:
     versionedModule['urllib'] = 'urllib2'
 '''
 
