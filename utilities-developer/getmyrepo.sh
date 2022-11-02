094223d3b5b9e83e3db42a2c3bc1ed8ddb75225d
poikilos <7557867+poikilos@users.noreply.github.com>
2022-11-01 21:01:40 -0400

    Also look for repos in hierosoft.

--- utilities-developer/getmyrepo.sh
+++ utilities-developer/getmyrepo.sh
@@ -2,12 +2,23 @@
 
 #region settings
 MY_GITHUB_USERNAME="poikilos"
+# ^ Changed further down if not present there.
 FORCE_GIT_DIR=~/git  # if this is blank, the repo will clone to the current directory.
 #endregion settings
 
 
 if [ -z "$1" ]; then
     "You must specify a repo name."
+fi
+if [ ! -z "$FORCE_GIT_DIR" ]; then
+    getrepo.sh "$@" --user $MY_GITHUB_USERNAME --repos_dir "$FORCE_GIT_DIR" --user_dir "$FORCE_GIT_DIR"
+else
+    getrepo.sh "$@" --user $MY_GITHUB_USERNAME --repos_dir "`pwd`" --user_dir "`pwd`"
+fi
+if [ $? -ne 0 ]; then
+    MY_GITHUB_USERNAME="hierosoft"
+else
+    exit 0
 fi
 if [ ! -z "$FORCE_GIT_DIR" ]; then
     getrepo.sh "$@" --user $MY_GITHUB_USERNAME --repos_dir "$FORCE_GIT_DIR" --user_dir "$FORCE_GIT_DIR"
