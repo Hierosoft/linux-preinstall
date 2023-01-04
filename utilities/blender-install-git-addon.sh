c30b0f586a3a70275c2abdf25d6fa7f08cc9f7af
poikilos <7557867+poikilos@users.noreply.github.com>
2023-01-03 20:37:24 -0500

    For Blender addons adder allow: --in-place install, install from local directory, and describe manual install for run-in-place versions.

--- utilities/blender-install-git-addon.sh
+++ utilities/blender-install-git-addon.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 customDie(){
     echo
@@ -15,30 +15,48 @@ if [ ! -d Downloads/git ]; then
 fi
 cd Downloads/git
 
+url=""
+src=""
+IN_PLACE=false
 
 for var in "$@"
 do
     if [[ $var == --* ]]; then
         if [ "$var" = "--uninstall" ]; then
             enable_uninstall=true
+        elif [ "$var" = "--in-place" ]; then
+            IN_PLACE=true
         else
             echo "'$var' is not a recognized option."
             exit 1
         fi
     else
-        url="$var"
+        if [ -d "$var" ]; then
+            if [ "x$src" != "x" ]; then
+                echo "The source directory was already set."
+                exit 1
+            fi
+            src="$var"
+        else
+            url="$var"
+        fi
     fi
 done
 
 if [ -z "$url" ]; then
-    customDie "No URL was specified (http* URL ending in .git or other valid git URL should be specified as param to this script)."
+    if [ -z "$src" ]; then
+        customDie "No directory nor URL was specified (http* URL ending in .git or other valid git URL should be specified as param to this script)."
+    fi
 fi
 
 project_dot_git=${url##*/}
 name=`echo "$project_dot_git" | cut -d'.' -f1`
 echo "$name"
-src=""
-if [ ! -d "$name" ]; then
+
+if [ "x$src" != "x" ]; then
+    name="`basename $src`"
+    echo "* installing local \"$src\" as $name"
+elif [ ! -d "$name" ]; then
     if [ ! -f "`command -v git`" ]; then
         customDie "For downloading, $0 requires git, but it is not present."
     fi
@@ -73,16 +91,26 @@ A path such as $HOME/.config/blender (or $HOME/.blender/2.79) must exist for thi
 END
     exit 1
 fi
-for sv in 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99
+
+for mv in 3 2
 do
-    version_path="$versions_path/2.$sv"
-    #echo "looking for $version_path..."
-    if [ -d "$version_path" ]; then
-        echo "found."
-        cd "$version_path"
-        version="2.$sv"
+    for sv in 3 2 1 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+    do
+        # Check for 3.3 first since that is stable (3.3.x use /3.3/ not /3.3.*/) 
+        version_path="$versions_path/$mv.$sv"
+        #echo "looking for $version_path..."
+        if [ -d "$version_path" ]; then
+            echo "* detected \"$version_path\""
+            cd "$version_path"
+            version="$mv.$sv"
+            break
+        fi
+    done
+    if [ "x$version" != "x" ]; then
+        break
     fi
 done
+
 version_path="$versions_path/$version"
 if [ ! -d "$version_path" ]; then
     echo
@@ -97,6 +125,10 @@ if [ ! -d "$addons" ]; then
     mkdir -p "$addons"
 fi
 echo
+if [ -z "$name" ]; then
+    echo "Error: The addon name wasn't set."
+    exit 1
+fi
 addon="$addons/$name"
 if [ "@$enable_uninstall" = "@true" ]; then
     echo "Uninstalling $src/* files from $addons..."
@@ -106,30 +138,64 @@ if [ "@$enable_uninstall" = "@true" ]; then
     find . -exec rm -rf $addons/{} \;
     cd "$prev_dir"
 else
+    src_param=""
     if [ -f "$src/__init__.py" ]; then
         echo "* installing the $name add-on to $addons as a new directory..."
         echo
         #rsync -rtv "$src/" "$addons"
-        if [ -d "$addon" ]; then
-            echo "Removing previously installed $addon..."
-            rm -Rf "$addon"
+        if [ -L "$addon" ]; then
+            echo "Removing previously installed $addon symlink..."
+            rm -f "$addon"
+        elif [ -d "$addon" ]; then
+            echo "Removing previously installed $addon directory..."
+            rm -Rvf "$addon"
         fi
-        cp -R $src "$addons/"
+        src_param="$src"
+        if [ "x$IN_PLACE" = "xtrue" ]; then
+            ln -s $src "$addons/"
+            echo "* created symlink in $addons/"
+            ls -la addons/`basename $src`
+        else
+            cp -Rv $src "$addons/"
+        fi
     else
         echo "* installing the $name add-on py files directly to $addons..."
         echo
         #rsync -rtv "$src/" "$addons"
-        if [ -d "$addon" ]; then
+        if [ -L "$addon" ]; then
+            echo "Removing incorrectly installed $addon symlink..."
+            rm -f "$addon"
+        elif [ -d "$addon" ]; then
             echo "Removing incorrectly installed $addon..."
             rm -Rf "$addon"
         fi
-        cp -R $src/* "$addons/"
+
+        src_param="$src/*"
+        if [ "x$IN_PLACE" = "xtrue" ]; then
+            ln -s $src/* "$addons/"
+            echo "* created symlinks in $addons/:"
+            ls $src
+        else
+            cp -R $src/* "$addons/"
+        fi
     fi
     echo
+    echo
     echo "You still need to enable the addon in Blender $version:"
-    echo "  * File, User Preferences"
-    echo "  * Addons"
-    echo "  * Check the box for $name"
+    echo "* Only the following versions of Blender will use the user $version/scripts directory:"
+    echo "  - The system's Blender: \"`command -v blender`\" (`blender --version | head -n1`)"
+    echo "  - Blender AppImages"
+    echo "  - Blender versions compiled from source but not run-in-place."
+    echo "* If you don't have $version in one of those formats, install in your version such as:"
+    try_addons="$HOME/Downloads/blender-3.3.1-linux-x64/3.3/scripts"
+    if [ "x$IN_PLACE" = "xtrue" ]; then
+        echo "ln -s $src_param \"$try_addons/\""
+    else
+        echo "cp -R $src_param \"$addons/\""
+    fi
+    echo "* Open the correct Blender above then File, User Preferences"
+    echo "* Addons"
+    echo "* Check the box for $name (or earch for \"DirectX Importer\" first)"
 fi
 echo
 echo
