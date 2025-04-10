925e25f76ea21b620d093a55d1eaa3cb94e79f0d
poikilos <7557867+poikilos@users.noreply.github.com>
2025-04-10 13:52:37 -0400

    Fix cinnamon panel refresh.

--- utilities/refresh-any-panel.nonroot.sh
+++ utilities/refresh-any-panel.nonroot.sh
@@ -2,15 +2,23 @@
 found_mate_panel="`ps aux | grep mate-panel | grep -v grep`"
 if [ ! -z "$found_mate_panel" ]; then
     mate-panel --replace
+    exit $?
 fi
 update-desktop-database
-# /home/owner/.local/share/applications/xfce4-appfinder.desktop
+# $HOME/.local/share/applications/xfce4-appfinder.desktop
 
-found_cinnamon="`ps | grep cinnamon`"
+found_cinnamon="`ps x | grep cinnamon | grep -v grep`"
+# x: all users
 # TODO: use a universal reloader (see code in )
 if [ ! -z "$found_cinnamon" ]; then
-    nohup cinnamon --replace & disown
+    # "r" doesn't work from terminal, so:
+    # nohup cinnamon --replace & disown
+    # Restart preserving open windows and running applications as per <https://askubuntu.com/a/523436>:
+    pkill -HUP -f "cinnamon --replace"
+    exit $?
 fi
+echo "Warning: The panel for your desktop environment was not detected. New icon(s) may not appear until GUI logout and login."
+exit 1
 
 
 # See <https://askubuntu.com/a/722713>
